963c011c1d30644e1ebc9397e532b5bb5677b382
fabioo66 <torrejonfabio@gmail.com>
2024-09-05 18:18:10 -0300

    ejercicio 1 y 2

---
+++ ejercicio2/src/main/java/ar/edu/info/unlp/ejercicioDemo/Balanza.java
@@ -0,0 +1,51 @@
+package ar.edu.info.unlp.ejercicioDemo;
+
+public class Balanza {
+    private int cantidadDeProductos;
+    private double precioTotal;
+    private double pesoTotal;
+
+    public Balanza () {
+        
+    }
+
+	public int getCantidadDeProductos() {
+		return cantidadDeProductos;
+	}
+
+	public void setCantidadDeProductos(int cantidadDeProductos) {
+		this.cantidadDeProductos = cantidadDeProductos;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	public double getPesoTotal() {
+		return pesoTotal;
+	}
+
+	public void setPesoTotal(double pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
+
+    public void ponerEnCero() {
+        this.cantidadDeProductos = 0;
+        this.precioTotal = 0;
+        this.pesoTotal = 0;
+    }
+
+    public void agregarProducto(Producto producto) {
+        this.cantidadDeProductos++;
+        this.precioTotal += producto.getPrecio();
+        this.pesoTotal += producto.getPeso();
+    }
+
+    public Ticket emitirTicket() {
+        return new Ticket(this.cantidadDeProductos, this.pesoTotal, this.precioTotal);
+    }
+}
